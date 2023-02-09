9b58fdb4042e3ff93f5b578089779dbcb5bef9d8
JEONG JEAGONG <jjkong86@gmail.com>
2023-02-09 15:33:58 +0900

    init

---
+++ src/test/java/com/me/springdata/mongodb/SpringDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.springdata.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
